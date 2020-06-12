1b5f6c925855dd1fd15cd5974a910c807e38a84b
billyh <billyhkaufman@outlook.com>
2020-06-12 18:42:24 -0400

    editted config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const APIURL = window.location.hostname === 'localhost' ? 'http://localhost:8000' : '';
+export const APIURL =
+					window.location.hostname === 'localhost'
+						? 'http://localhost:8000'
+						: 'http://localhost:8000';
