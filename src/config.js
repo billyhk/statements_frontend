d65ed46bb3d7d101723c9441824379fc585e47e1
billyh <billyhkaufman@outlook.com>
2020-06-12 20:32:34 -0400

    editted config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const APIURL =
 					window.location.hostname === 'localhost'
 						? 'http://localhost:8000'
-						: 'http://fierce-escarpment-04103.herokuapp.com/';
+						: 'http://fierce-escarpment-04103.herokuapp.com';
