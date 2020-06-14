baca3a0bafaefaf32166ed2bc09bb39feaa7ce90
billyh <billyhkaufman@outlook.com>
2020-06-14 13:14:59 -0400

    edited bottom padding for various screen sizes

--- src/config.js
+++ src/config.js
@@ -2,3 +2,5 @@ export const APIURL =
 					window.location.hostname === 'localhost'
 						? 'http://localhost:8000'
 						: 'http://fierce-escarpment-04103.herokuapp.com';
+
+// export const APIURL = 'http://fierce-escarpment-04103.herokuapp.com';
