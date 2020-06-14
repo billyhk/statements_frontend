27711fbb61436b106072e48b98b38710e8b24379
billyh <billyhkaufman@outlook.com>
2020-06-14 11:19:34 -0400

    used older syntax for nav icons so they appear in chrome

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -39,7 +39,7 @@ const NavBar = ({ userToken, handleSignOut }) => {
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='ml-auto' onClick={closeNav}>
 						<Link to='/'>
-							<i className='fas fa-home'></i>
+							<i className='fa fa-home'></i>
 							<span className='toggle-hidden-nav' id='toggle-hidden-nav'>
 								Home
 							</span>
@@ -47,29 +47,29 @@ const NavBar = ({ userToken, handleSignOut }) => {
 						{userToken ? (
 							<>
 								<Link to='/user'>
-									<i className='fas fa-user'></i>{' '}
+									<i className='fa fa-user'></i>{' '}
 									<span className='toggle-hidden-nav'>My Account</span>
 								</Link>
 								<Link to='/user/new-transaction'>
-									<i className='fas fa-plus'></i>{' '}
+									<i className='fa fa-plus'></i>{' '}
 									<span className='toggle-hidden-nav'>New Transaction</span>
 								</Link>
 								<Link to='/user/all-transactions'>
-									<i className='fas fa-wallet'></i>{' '}
+									<i className='fa fa-wallet'></i>{' '}
 									<span className='toggle-hidden-nav'>All Transactions</span>
 								</Link>
 								<Link to='/user/financial-statements-summary'>
-									<i className='fas fa-receipt'></i>{' '}
+									<i className='fa fa-receipt'></i>{' '}
 									<span className='toggle-hidden-nav'>Financial Statements</span>
 								</Link>
 								<Link to='/' onClick={handleSignOut}>
-									<i className='fas fa-lock-open'></i>{' '}
+									<i className='fa fa-lock-open'></i>{' '}
 									<span className='toggle-hidden-nav'>SignOut</span>
 								</Link>
 							</>
 						) : (
 							<Link to='/signin'>
-								<i className='fas fa-lock'></i>
+								<i className='fa fa-lock'></i>
 								<span className='toggle-hidden-nav'> Sign In</span>
 							</Link>
 						)}
